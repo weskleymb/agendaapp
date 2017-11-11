f026f971cfd913ffb7a60e2ecd5d9991edf6d2ea
Weskley Bezerra <weskleymb@hotmail.com>
2017-11-10 21:01:16 -0300

    Commit Inicial

--- app/src/main/java/br/senac/rn/agendaescolar/views/AlunoListaActivity.java
+++ app/src/main/java/br/senac/rn/agendaescolar/views/AlunoListaActivity.java
@@ -1,10 +1,16 @@
 package br.senac.rn.agendaescolar.views;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -74,6 +80,7 @@ public class AlunoListaActivity extends AppCompatActivity {
                 alunos);
 
         lvAlunos.setAdapter(adapter);
+        registerForContextMenu(lvAlunos);
     }
 
     private void definirEventos() {
@@ -89,4 +96,39 @@ public class AlunoListaActivity extends AppCompatActivity {
 //        });
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lista_opcoes, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo alunoEscolhido;
+        alunoEscolhido = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        Aluno aluno = (Aluno) lvAlunos.getItemAtPosition(alunoEscolhido.position);
+        switch (item.getItemId()) {
+            case R.id.item_sms:
+                Intent intentSms = new Intent(Intent.ACTION_VIEW);
+                intentSms.setData(Uri.parse("sms:" + aluno.getFone()));
+                item.setIntent(intentSms);
+                break;
+            case R.id.item_site:
+                Intent intentSite = new Intent(Intent.ACTION_VIEW);
+                intentSite.setData(Uri.parse(aluno.getSite()));
+                item.setIntent(intentSite);
+                break;
+            case R.id.item_mapa:
+                Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+                intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+                item.setIntent(intentMapa);
+                break;
+            case R.id.item_ligar:
+                Intent intentLigar = new Intent(Intent.ACTION_VIEW);
+                intentLigar.setData(Uri.parse("tel:" + aluno.getFone()));
+                item.setIntent(intentLigar);
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
 }
