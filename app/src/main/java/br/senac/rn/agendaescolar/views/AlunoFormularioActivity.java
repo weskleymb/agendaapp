494420a0495599fdcb5f45a48b5966204ae95a83
Weskley Bezerra <weskleymb@hotmail.com>
2017-11-13 20:51:10 -0300

    Commit Inicial

---
+++ app/src/main/java/br/senac/rn/agendaescolar/views/AlunoFormularioActivity.java
@@ -0,0 +1,13 @@
+package br.senac.rn.agendaescolar.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlunoFormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno_formulario);
+    }
+}
